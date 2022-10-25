97c6fdf715bf3622616d28e50f88da8367d8e2ce
mendsalbert <mendsalbert@gmail.com>
2022-10-25 22:55:19 +0000

    dashboard

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
+import axios from "axios";
 
+axios.defaults.baseURL = "https://freshpick-server.herokuapp.com";
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
